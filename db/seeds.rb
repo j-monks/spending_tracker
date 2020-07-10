49dba2fc65525711e735fb857f510c5bf9e8541f
James Monks <J.monks23@gmail.com>
2020-07-10 14:51:39 +0100

    category successfully saving to database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 require_relative("../models/merchant")
+require_relative("../models/category")
 require("pry-byebug")
 
 merchant1 = Merchant.new({
@@ -6,8 +7,14 @@ merchant1 = Merchant.new({
 })
 merchant1.save()
 
+category1 = Category.new({
+    "name" => "Groceries"
+})
+category1.save()
+
 merchant1.name = "Asda"
 merchant1.update()
+
 
 binding.pry
 nil
