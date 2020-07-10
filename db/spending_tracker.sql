7bf64d7052b8e3ff1523cfd3d1d7febe177a17e3
James Monks <J.monks23@gmail.com>
2020-07-10 14:45:35 +0100

    added categories table

--- db/spending_tracker.sql
+++ db/spending_tracker.sql
@@ -4,4 +4,10 @@ CREATE TABLE merchants
 (
     id SERIAL primary key,
     name VARCHAR(255) not null
-)
+);
+
+CREATE TABLE categories
+(
+    id SERIAL primary key,
+    name VARCHAR(255) not null
+);
--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -1,10 +1,10 @@
-require( 'pg' )
+require("pg")
 
 class SqlRunner
 
   def self.run( sql, values = [] )
     begin
-      db = PG.connect({ dbname: 'spending_tracker', host: 'localhost' })
+      db = PG.connect({ dbname: "spending_tracker", host: "localhost" })
       db.prepare("query", sql)
       result = db.exec_prepared( "query", values )
     ensure
