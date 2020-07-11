ec3d3411d80354ac091de5edf6fe92aa68e04b63
James Monks <J.monks23@gmail.com>
2020-07-11 12:05:52 +0100

    create new category page added allowing you to create a category and add it to the db

--- controllers/category_controller.rb
+++ controllers/category_controller.rb
@@ -7,3 +7,12 @@ get "/categories" do
   @categories = Category.all()  
   erb(:"categories/index")
 end
+
+get "/new-category" do
+    erb(:"categories/new")
+end
+
+post "/categories" do
+    Category.new(params).save()
+    redirect to "/categories"
+end
